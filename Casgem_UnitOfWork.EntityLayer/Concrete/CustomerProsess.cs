1cfb4e3b7bf4d5a93cafa7f3f9a4fc478322923f
selviyldrm <selviyldrm96@gmail.com>
2023-07-26 12:59:20 +0300

    Add project files.

---
+++ Casgem_UnitOfWork.EntityLayer/Concrete/CustomerProsess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem_UnitOfWork.EntityLayer.Concrete
+{
+    public class CustomerProsess
+    {
+        public int CustomerProcessID { get; set; }
+        public int SenderID { get; set; }
+        public int ReceiverID { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+}
